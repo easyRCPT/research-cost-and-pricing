04037467db984af969a8df03ef2d5a73ffb985fd
chenhaol <chenhaol@student.unimelb.edu.au>
2026-09-04 19:06:51 +1000

    Add budget update serializer and service skeleton

--- backend/api/services/budget_update.py
+++ backend/api/services/budget_update.py
@@ -1,11 +1,33 @@
+from ..models import Budget
 from . import budget_details
 
-def update_budget(budget, request) -> dict | None:
+
+def update(budget: Budget, data: dict) -> dict | None:
     requires_calculation = False
 
     # Update database
+    # section = data["section"]
+    # row_id = data.get("row_id")
+    # field = data["field"]
+    # value = data["value"]
 
     if requires_calculation:
         return budget_details.get_budget_details(budget)
 
     return None
+
+
+def update_project(budget: Budget, field: str, value: object) -> bool:
+    return False
+
+
+def update_budget(budget: Budget, field: str, value: object) -> bool:
+    return False
+
+
+def update_staff(budget: Budget, row_id: int, field: str, value: object) -> bool:
+    return False
+
+
+def update_non_staff(budget: Budget, row_id: int, field: str, value: object) -> bool:
+    return False
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,31 +1,38 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Budget
-from .serializers import ProjectDetailSerializer
+from .serializers.budget_detail_serializer import BudgetDetailSerializer
+from .serializers.budget_update_serializer import BudgetUpdateSerializer
 from .services import budget_details, budget_update
 
 
 class BudgetDetailView(APIView):
-    def get(self, request, budget_id):
+    def get(self, request: Request, budget_id: int) -> Response:
         budget = get_object_or_404(Budget, id=budget_id)
 
         result = budget_details.get_budget_details(budget)
 
-        serializer = ProjectDetailSerializer(result)
+        serializer = BudgetDetailSerializer(result)
 
         return Response(serializer.data)
 
-
-    def patch(self, request, budget_id):
+    def patch(self, request: Request, budget_id: int) -> Response:
         budget = get_object_or_404(Budget, id=budget_id)
 
-        result = budget_update.update_budget(budget, request.data)
+        serializer = BudgetUpdateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        result = budget_update.update(budget, serializer.validated_data)
 
         if result is not None:
-            serializer = ProjectDetailSerializer(result)
-            return Response(serializer.data)
+            result_serializer = BudgetDetailSerializer(result)
+            return Response(
+                result_serializer.data,
+                status=status.HTTP_200_OK,
+            )
 
         return Response(status=status.HTTP_204_NO_CONTENT)
