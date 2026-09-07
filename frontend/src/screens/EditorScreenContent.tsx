689c6be5fb7c90b4c9f02f2e8d04fc9b934fe0da
Abbas Hasan <abbas.hasan7126@gmail.com>
2026-09-07 10:05:29 +1000

    feat(editor): route to the pricing and authorisation screens

--- frontend/src/screens/EditorScreenContent.tsx
+++ frontend/src/screens/EditorScreenContent.tsx
@@ -3,6 +3,9 @@ import type { LookupTables, ProjectInfo } from '@/types'
 import { ProjectDetails } from './ProjectDetails'
 import { NonStaffCosts, type NonStaffCostsProps } from './NonStaffCosts'
 import { StaffCosts } from './StaffCosts'
+import { PriceSummary } from './PriceSummary'
+import { InKind } from './InKind'
+import { BudgetForm } from './BudgetForm'
 import { EmptyStateScreen } from './EmptyStateScreen'
 
 interface EditorScreenContentProps {
@@ -33,6 +36,14 @@ export function EditorScreenContent({
       )
     case 'staff':
       return <StaffCosts budgetId={budgetId} lookups={lookups} />
+    case 'inkind':
+      return (
+        <InKind budgetId={budgetId} lookups={lookups} nonStaff={nonStaff} />
+      )
+    case 'price':
+      return <PriceSummary budgetId={budgetId} lookups={lookups} />
+    case 'budget':
+      return <BudgetForm budgetId={budgetId} lookups={lookups} />
     case 'nonstaff':
       return <NonStaffCosts {...nonStaff} lookups={lookups} />
     default:
